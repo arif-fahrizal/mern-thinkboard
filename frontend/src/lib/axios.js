043a7c4faf448865e0c290322f629c07b8bdb632
arif-fahrizal <fahrizalarif07@gmail.com>
2025-12-02 23:05:20 +0700

    [FIX] import env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.URL_ENDPOINT}/api`,
+  baseURL: `${import.meta.env.VITE_URL_ENDPOINT}/api`,
 });
 
 export default api;
